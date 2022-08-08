d473874cc06203ae846f548687b9616f12d47f53
DadaLV <vynohradova.l.a@gmail.com>
2022-08-08 20:05:25 +0300

    modal for header added

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+function hide() {
+  const modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+function showText() {
+  let headerText;
+  if (window.innerWidth < 1026) {
+    headerText = "Check delivery";
+  } else {
+    headerText =
+      "We're focusing on London to start with, please enter your post code here to see if you're within our delivery zone!";
+  }
+  document.querySelector(".header_text").innerHTML = headerText;
+}
+window.addEventListener("resize", showText);
