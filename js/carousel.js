1d78e6249056f210224a3e33150ac60419715289
DadaLV <vynohradova.l.a@gmail.com>
2022-08-13 03:15:55 +0300

    carousel js add

--- js/carousel.js
+++ js/carousel.js
@@ -1,12 +1,12 @@
 (function () {
   const slides = [
-    `<img class ="carouselImg" src="./images/first_carousel/carouselImg0.png" alt="carouselImg0">`,
-    `<img class ="carouselImg" src="./images/first_carousel/carouselImg1.png" alt="carouselImg1">`,
-    `<img class ="carouselImg" src="./images/first_carousel/carouselImg2.png" alt="carouselImg2">`,
-    `<img class ="carouselImg" src="./images/first_carousel/carouselImg4.png" alt="carouselImg4">`,
-    `<img class ="carouselImg" src="./images/first_carousel/carouselImg5.png" alt="carouselImg5">`,
-    `<img class ="carouselImg" src="./images/first_carousel/carouselImg6.png" alt="carouselImg6">`,
-    `<img class ="carouselImg" src="./images/first_carousel/carouselImg7.png" alt="carouselImg7">`,
+    `<img class ="carouselImg" src="img/first_carousel/carouselImg0.png" alt="carouselImg0">`,
+    `<img class ="carouselImg" src="img/first_carousel/carouselImg1.png" alt="carouselImg1">`,
+    `<img class ="carouselImg" src="img/first_carousel/carouselImg2.png" alt="carouselImg2">`,
+    `<img class ="carouselImg" src="img/first_carousel/carouselImg4.png" alt="carouselImg4">`,
+    `<img class ="carouselImg" src="img/first_carousel/carouselImg5.png" alt="carouselImg5">`,
+    `<img class ="carouselImg" src="img/first_carousel/carouselImg6.png" alt="carouselImg6">`,
+    `<img class ="carouselImg" src="img/first_carousel/carouselImg7.png" alt="carouselImg7">`,
   ];
 
   let currentSlide = 0;
