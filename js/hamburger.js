6ef08c68b9016e8778fc8ec72933fb632c3f7453
DadaLV <vynohradova.l.a@gmail.com>
2022-08-13 03:16:14 +0300

    hamburger menu added

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,19 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navbar = document.querySelector(".navbar");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  navbar.classList.toggle("active");
+
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
+  navbar.classList.remove("active");
 }))
+
+function lockScroll() {
+  document.body.classList.toggle('lock-scroll');
+}
